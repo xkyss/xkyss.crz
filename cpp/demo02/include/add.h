0fd8de3e395476ace871f7c36284cdf2bd24f9ca
xkyii@th <xkyii@foxmail.com>
2025-06-19 10:54:59 +0800

    Add: demo02
    exe依赖lib

---
+++ cpp/demo02/include/add.h
@@ -0,0 +1,10 @@
+//
+// Created by dev88 on 2025/6/19.
+//
+
+#ifndef ADD_H
+#define ADD_H
+
+int add(int a, int b);
+
+#endif //ADD_H
---
+++ cpp/demo02/sample/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <add.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << " 5 + 10 = " << add(5, 10) << std::endl;
+    return 0;
+}
