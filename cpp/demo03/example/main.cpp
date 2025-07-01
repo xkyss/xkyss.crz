c6a058c606c97a7c53bf7912bd9c4a5a57cb86b5
xkyii@th <xkyii@foxmail.com>
2025-07-01 17:18:30 +0800

    Up: demo03  C++模板 cmake/导出c api

--- cpp/demo03/example/main.cpp
+++ cpp/demo03/example/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Paper.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    const auto p = new demo03::Paper();
+    p->foo01();
     return 0;
 }
